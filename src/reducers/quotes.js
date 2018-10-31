33d422facd9b2923ed9a6fc12a4bcf6b0ddc79da
Sharmina Akhtar <sakht318@gmail.com>
2018-10-31 16:47:33 +0000

    automatically backed up by learn

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,39 @@
 export default (state = [], action) => {
   return state;
 }
+
+export function quotes(state = [], action){
+  switch (action.type) {
+  case "ADD_QUOTE":
+    return [].concat(state, action.payload)
+  case "REMOVE_QUOTE":
+    let idx = state.cuid()
+    return [].concat(state.slice(0, idx), state.slice(idx - 1, state.length))
+  default:
+    return state
+  }
+}
+
+
+export function voteQuotes(state = [], action){
+  switch (action.type) {
+  case "UPVOTE_QUOTE":
+    return [].concat(state, action.payload)
+  case "DOWNVOTE_QUOTE":
+    let idx = state.cuid()
+    return [].concat(state.slice(0, idx), state.slice(idx - 1, state.length))
+  default:
+    return state
+  }
+}
+
+export function combineReducers(reducers){
+  return (state = {}, action) => {
+    return Object.keys(reducers).reduce(
+      (nextState, key)=>{
+        nextState[key] = reducers[key](state[key], action);
+        return nextState
+      }, {}
+    )
+  }
+}
